d70ab5a59a97b2355f53737efa2d710a4bfb54be
Ian Luria <43768078+ianluria@users.noreply.github.com>
2018-10-11 15:24:24 -0600

    Added YouTube Embedding JavaScript Code

--- productLanding.js
+++ productLanding.js
@@ -1,6 +1,33 @@
 
 addNavLinks(["Description", "Video", "Request"]);
 
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 't1nRXlsUKFs',
+        playerVars: {
+            controls: '0',
+            modestbranding: "0",
+            origin: "http://127.0.0.1:8080/productLanding.html"
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.mute();
+}
+
 function addNavLinks(arrayOfTitles) {
 
     arrayOfTitles.forEach(title => {
@@ -16,7 +43,4 @@ function onClickHandler(event) {
     document.getElementById("product" + event.target.innerText).scrollIntoView({ block: "start", behavior: "smooth" });
 }
 
-const youTubePlayer = document.getElementById("youTubePlayer");
-
-console.log(youTubePlayer);
 
